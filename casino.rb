526cba32fb5ba6e247bf30d47115ebee85762785
joshua-murphy <josh.murphy11@gmail.com>
2017-11-13 10:36:46 -0700

    add menu

--- casino.rb
+++ casino.rb
@@ -1,12 +1,28 @@
 require "pry"
 require_relative 'player'
 
+
 class Casino
 	attr_accessor :player
 
 	def initialize
 		puts "Welcome to the Ruby Casino"
 		@player = Player.new
+		menu
+	end
+
+	def menu
+		@options = ["Blackjack", "Exit"]		
+		@options.each_with_index { |opt, i| puts "#{i + 1}) #{opt}" }
+		choice = gets.to_i - 1
+		case choice
+			when 1
+				puts "Come again!"
+				exit
+			else
+				puts "Invalid choice"
+				menu
+		end
 	end
 end
 
